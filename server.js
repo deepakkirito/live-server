c19ec57fe42b36f0b9d4d2a258b9879b94258076
Glitch (liveserver) <none>
2023-01-24 09:05:16 +0000

    #️⃣☝️ Updated with Glitch

--- server.js
+++ server.js
@@ -8,9 +8,29 @@ const serveIndex = require('serve-index');
 const app = express();
 const routes = require('./Routes/api-routes')
 
+const fastify = require("fastify")({
+  logger: false,
+});
+
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb+srv://deepakkirito:Deep%40kkirito@cluster0.otcmztf.mongodb.net/test');
 let db = mongoose.connection;
+
+// Setup our static files
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/", // optional: default '/'
+});
+
+// fastify-formbody lets us parse incoming forms
+fastify.register(require("@fastify/formbody"));
+
+// point-of-view is a templating manager for fastify
+fastify.register(require("@fastify/view"), {
+  engine: {
+    handlebars: require("handlebars"),
+  },
+});
 
 app.use('/public', express.static(path.join(__dirname, './Public')));
 app.use('/public', serveIndex(path.join(__dirname, './Public')));
@@ -35,8 +55,16 @@ db.on('open', () => {
     startServer();
 });
 
+// Run the server and report out to the logs
 const startServer = () => {
-    app.listen(7000, () => {
-        console.log(`Server is running at: http://localhost:7000`);
-    })
-};
+  app.listen(
+  { port: process.env.PORT, host: "0.0.0.0" },
+  function (err, address) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Your app is listening on ${address}`);
+  }
+);
+}
